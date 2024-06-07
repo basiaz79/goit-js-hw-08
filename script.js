d4c6d42df679291ffac34fe6640e971eecab4f0b
basiaz79 <bzasowski79@gmail.com>
2024-06-07 23:46:42 +0200

    hw module 8

---
+++ script.js
@@ -0,0 +1,45 @@
+// Analiza pliku JSON
+// const pies = {
+//     imie: "Azor", // parametr imie zapisany jako tekst (string)
+//     wiek: 8, //parament wiek  zapisanym jako liczba (number)
+//     czySzczeka: true,
+//     szczeka() {
+//         console.log("hauhau");
+//     }
+// }
+
+// Analiza Metody JSON.parse()
+const json = '{"name":"Mango","age":3,"isHappy":true}';
+const obj = JSON.parse(json);
+console.log(obj);
+
+// wynik w konsoli: {name: 'Mango', age: 3, isHappy: true}
+console.log(JSON.parse(json))
+
+
+// Obsługa błędów
+try {
+    // kod który powinien się wykonać bez błędów
+    const dane = JSON.parse("Jakis, dziwny format json");
+
+} catch (error) {
+    // wyłapywanie błędów
+    console.log(error.name);
+    console.log(error.message);
+}
+
+let dane
+let zewnetrznySerwis = '{"Jakis, dziwny format json"}';
+
+try {
+//     // kod ktory powinien sie wykonac bez bledow
+    dane = JSON.parse(zewnetrznySerwis);
+
+} catch (error) {
+    // wylapywanie bledow
+    console.error(error);
+    dane = '{"imie":"","wiek":,"czySzczeka":}'
+}
+
+
+// console.log(dane);
